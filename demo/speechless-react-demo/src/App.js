3854179588e1583dd9ee4c99f79dd3d7b4672a25
Shy <puemos@gmail.com>
2017-12-12 07:18:12 +0100

    3 diff inputs

--- demo/speechless-react-demo/src/App.js
+++ demo/speechless-react-demo/src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 import Header from './Components/Header'
 import Input from './Components/Input'
 import './App.css'
+import { RecognitionFactory, ExternalRecognition, NativeRecognition } from 'speechless'
+import { getRecognition } from './api'
 
 class App extends Component {
   render() {
@@ -10,8 +12,19 @@ class App extends Component {
       <div className="App">
         <Header />
         <div className="Body column center">
-        <div className="row center">
-          <Input />
+          <div className="column end">
+            <div className="row center">
+              <h3>Auto detect</h3>
+              <Input recognition={RecognitionFactory('en', getRecognition)} />
+            </div>
+            <div className="row center">
+              <h3>External</h3>
+              <Input recognition={new ExternalRecognition('en', getRecognition)} />
+            </div>
+            <div className="row center">
+              <h3>Native</h3>
+              <Input recognition={new NativeRecognition('en')} />
+            </div>
           </div>
         </div>
       </div>
--- demo/speechless-react-demo/src/Components/Input/index.js
+++ demo/speechless-react-demo/src/Components/Input/index.js
@@ -1,63 +1,17 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import { RecognitionFactory, ExternalRecognition } from 'speechless'
 
 import loader from '../../Assets/loader.svg'
 import recording from '../../Assets/recording.svg'
 import mic from '../../Assets/mic.svg'
 
 import './style.css'
-import { AbstractRecognition } from 'speechless/dist/types/AbstractRecognition'
 
-function remoteCall(content) {
-  return fetch(
-    'https://content-speech.googleapis.com/v1/speech:recognize?alt=json&key=AIzaSyBW2ROnSfou8fo0J7Pa-B2uPq3U45V-jnk',
-    {
-      method: 'post',
-      body: JSON.stringify({
-        config: {
-          encoding: 'LINEAR16',
-          sampleRateHertz: 44100,
-          languageCode: 'en',
-          maxAlternatives: 1
-        },
-        audio: {
-          content
-        }
-      })
-    }
-  )
-}
-function toBase64(blob) {
-  var reader = new window.FileReader()
-  return new Promise(resolve => {
-    reader.readAsDataURL(blob)
-    reader.onloadend = function() {
-      resolve(reader.result)
-    }
-  })
-}
-function isEmpty(obj) {
-  for (var key in obj) {
-    if (obj.hasOwnProperty(key)) return false
-  }
-  return true
-}
-const getRecognition = blob =>
-  toBase64(blob)
-    .then(base64data => base64data.substr(base64data.indexOf(',') + 1))
-    .then(remoteCall)
-    .then(res => res.json())
-    .then(res => {
-      if (!res || isEmpty(res)) {
-        return ''
-      }
-      return res.results[0].alternatives[0].transcript
-    })
 
-export default class Header extends PureComponent {
+
+export default class Input extends PureComponent {
   static propTypes = {
-    recognition: PropTypes.instanceOf(AbstractRecognition)
+    recognition: PropTypes.object
   }
   state = {
     data: '',
@@ -69,13 +23,6 @@ export default class Header extends PureComponent {
   }
   componentDidMount = () => {
     const { recognition } = this.props
-    recognition.addEventListener('data', this.onData)
-    recognition.addEventListener('fetching', this.onFetching)
-    recognition.addEventListener('start', this.onStart)
-    recognition.addEventListener('end', this.onEnd)
-  }
-  componentWillReceiveProps = nextProps => {
-    const { recognition } = nextProps
     recognition.addEventListener('data', this.onData)
     recognition.addEventListener('fetching', this.onFetching)
     recognition.addEventListener('start', this.onStart)
